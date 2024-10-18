d074991b451ef28e41bd5b1ada75bfbed9e062d0
Hunter Baker <bakerhb@dukes.jmu.edu>
2024-10-17 21:23:28 -0400

    fresh wipe
    wiped

--- test.py
+++
@@ -1,9 +0,0 @@
-from managers.setting_server_manager import toggleTime
-
-# next i want to complete the server adding times
-# then the determining what servers are in each time slot
-
-SID = '12345'
-
-toggleTime(SID, "3", "12")
-
